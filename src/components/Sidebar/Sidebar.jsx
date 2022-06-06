1249c849ba8daa08ec600b979f41c56004fca52f
21ciprian <2021ciprianoffice@gmail.com>
2022-06-06 13:06:36 +0100

    Added link to Home page from Sidebar component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import styles from './Sidebar.module.css'
 
 function Sidebar() {
-	return <section className={styles.sidebar}>Sidebar</section>
+	return (
+		<section className={styles.sidebar}>
+			<div className={styles.top}>
+				<Link to='/' style={{textDecoration: 'none'}}>
+					BookingAdmin
+				</Link>
+			</div>
+		</section>
+	)
 }
 
 export default Sidebar
