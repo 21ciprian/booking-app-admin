366f7900135790b2dcadc4457dab2d370daf5880
21ciprian <2021ciprianoffice@gmail.com>
2022-06-06 13:00:33 +0100

    Created Sidebar component

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './Sidebar.module.css'
+
+function Sidebar() {
+	return <section className={styles.sidebar}>Sidebar</section>
+}
+
+export default Sidebar
